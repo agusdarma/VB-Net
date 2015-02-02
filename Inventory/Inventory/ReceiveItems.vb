6d9cc4786d312a9357ada4d9b6bfcde343db0ff2
Agus Darma Kusuma <agusdk2011@gmail.com>
2015-02-02 23:36:47 +0700

    sa

--- Inventory/Inventory/ReceiveItems.vb
+++ Inventory/Inventory/ReceiveItems.vb
@@ -32,7 +32,8 @@ Public Class ReceiveItems
         End Try
     End Sub
     Private Sub ReceiveItems_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        inisialisasi()
+        TextBoxReceiptNo.Focus()
     End Sub
     Private Sub inisialisasi()
         Dim now As DateTime = DateTime.Now
@@ -50,8 +51,8 @@ Public Class ReceiveItems
         Me.DataGridViewRI.Columns(1).Name = "Nama Item"
         Me.DataGridViewRI.Columns(2).Name = "Qty"
         Me.DataGridViewRI.Columns(3).Name = "Satuan"
-        Me.DataGridViewRI.Columns(4).Name = "Gudang"
-        Me.DataGridViewRI.Columns(5).Name = "PO Ref"
+        Me.DataGridViewRI.Columns(4).Name = "PO Ref"
+        Me.DataGridViewRI.Columns(5).Name = "Gudang"
     End Sub
     Private Function getPrimaryId() As Integer
         Dim nonqueryCommand As MySqlCommand
@@ -87,4 +88,135 @@ Public Class ReceiveItems
         kodeSupplier = CmbVendor.SelectedValue
         SearchPOForm.ShowDialog()
     End Sub
+    Public Sub addSelectPOToList()
+        Dim listPO = New List(Of PoVO)
+        listPO = SearchPOForm.listSelectPo
+        DataGridViewRI.Rows.Clear()
+        If DataGridViewRI.Columns.Count = 7 Then
+            DataGridViewRI.Columns.RemoveAt(DataGridViewRI.Columns.Count - 1)
+        End If
+
+        DataGridViewRI.Refresh()
+        For Each item As PoVO In listPO
+            findItemsByPONo(item.NOPO)
+        Next
+        Dim cmb As New DataGridViewComboBoxColumn()
+        cmb.HeaderText = "Nama Gudang"
+        cmb.Name = "CmbGudang"
+        Dim dt As DataTable = New DataTable()
+        dt = getListGudang()
+        If dt.Rows.Count > 0 Then
+            cmb.ValueMember = "id"
+            cmb.DisplayMember = "nama_gudang"
+            cmb.DataSource = dt
+            cmb.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells
+            DataGridViewRI.Columns.Add(cmb)
+            DataGridViewRI.Columns(5).Visible = False
+            DataGridViewRI.Columns(2).DefaultCellStyle.BackColor = Color.Aquamarine
+            For Each oItem As DataGridViewRow In DataGridViewRI.Rows
+                oItem.Cells("CmbGudang").Value = CInt(oItem.Cells(5).Value)
+                oItem.Cells(0).ReadOnly = True
+                oItem.Cells(1).ReadOnly = True
+                oItem.Cells(3).ReadOnly = True
+                oItem.Cells(4).ReadOnly = True
+            Next
+        End If        
+        DataGridViewRI.Refresh()
+    End Sub
+    Private Function getListGudang() As DataTable
+        Dim sql As String
+        Dim sqlCommand As New MySqlCommand
+        Dim publictable As New DataTable
+        Try
+            con = jokenconn()
+            con.Open()
+            sql = "select id,nama_gudang from gudang order by nama_gudang asc"
+            sqlCommand.Connection = con
+            sqlCommand.CommandText = sql
+            da.SelectCommand = sqlCommand
+            da.Fill(publictable)
+            con.Close()
+        Catch ex As MySqlException
+            MessageBox.Show("error : " + ex.ToString)
+        Finally
+            con.Close()
+        End Try
+        Return publictable
+    End Function
+    Private Sub findItemsByPONo(poNo As String)
+
+        Dim sql As String
+        Dim sqlCommand As New MySqlCommand
+        Dim publictable As New DataTable
+        Try
+            con = jokenconn()
+            con.Open()
+            sql = "select pd.kode_item,pd.nama_item,pd.qty,pd.satuan,g.id,ph.po_no from purchase_order_header ph left join purchase_order_detail pd on ph.id = pd.po_header_id left join items i on i.kode_item = pd.kode_item left join gudang g on g.id = i.gudang_id where ph.po_no ='" & poNo & "'"
+            sqlCommand.Connection = con
+            sqlCommand.CommandText = sql
+            da.SelectCommand = sqlCommand
+            da.Fill(publictable)
+            con.Close()
+            If publictable.Rows.Count > 0 Then                
+                Dim row As String()
+                For Each oRecord As Object In publictable.Rows
+                    row = New String() {oRecord("kode_item").ToString(), oRecord("nama_item").ToString(), oRecord("qty").ToString(), oRecord("satuan").ToString(), oRecord("po_no").ToString(), oRecord("id").ToString()}
+                    DataGridViewRI.Rows.Add(row)
+                Next                
+            End If
+        Catch ex As MySqlException
+            MessageBox.Show("error : " + ex.ToString)
+        Finally
+            con.Close()
+        End Try
+    End Sub
+
+    Private Sub DataGridViewRI_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridViewRI.CellClick
+        If e.ColumnIndex = 2 Then
+            DataGridViewRI.ClearSelection()
+            DataGridViewRI.Rows(e.RowIndex).Cells(e.ColumnIndex).Selected = True
+            ' Set the current cell to the cell in column 2, Row 0. 
+            DataGridViewRI.CurrentCell = Me.DataGridViewRI(e.ColumnIndex, e.RowIndex)
+            DataGridViewRI.BeginEdit(True)
+            'ElseIf e.ColumnIndex = 6 Then
+            '    DataGridViewRI.ClearSelection()
+            '    DataGridViewRI.Rows(e.RowIndex).Cells(e.ColumnIndex).Selected = True
+            '    ' Set the current cell to the cell in column 1, Row 0. 
+            '    DataGridViewRI.CurrentCell = Me.DataGridViewRI(e.ColumnIndex, e.RowIndex)
+            '    DataGridViewRI.BeginEdit(True)
+        End If
+    End Sub
+
+    Private Sub DataGridViewRI_EditingControlShowing(sender As Object, e As DataGridViewEditingControlShowingEventArgs) Handles DataGridViewRI.EditingControlShowing
+        e.CellStyle.BackColor = Color.Aquamarine
+    End Sub
+
+    Private Sub CmbVendor_SelectionChangeCommitted(sender As Object, e As EventArgs) Handles CmbVendor.SelectionChangeCommitted
+        findSupplierByKode(CmbVendor.SelectedValue)
+    End Sub
+    Private Function findSupplierByKode(kode As String) As Integer
+
+        Dim db As Integer
+        Dim sql As String
+        Dim cmd As New MySqlCommand
+        Dim publictable As New DataTable
+        Try
+            con = jokenconn()
+            sql = "select address1,address2,kode_supplier,name_supplier from supplier where kode_supplier ='" & kode & "'"
+            With cmd
+                .Connection = con
+                .CommandText = sql
+            End With
+            da.SelectCommand = cmd
+            da.Fill(publictable)
+            If publictable.Rows.Count > 0 Then
+                alamatVendor.Text = publictable.Rows(0).Item(0) + " " + publictable.Rows(0).Item(1)
+            End If
+        Catch ex As MySqlException
+            MessageBox.Show("error : " + ex.ToString)
+        Finally
+            con.Close()
+        End Try
+        Return db
+    End Function
 End Class
---
+++ Inventory/Inventory/SearchPOForm.Designer.vb
@@ -0,0 +1,131 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class SearchPOForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle5 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle6 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle7 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle8 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.DataGridViewListPO = New System.Windows.Forms.DataGridView()
+        Me.ButtonOk = New System.Windows.Forms.Button()
+        Me.ButtonCancel = New System.Windows.Forms.Button()
+        Me.CheckBoxCheckAll = New System.Windows.Forms.CheckBox()
+        CType(Me.DataGridViewListPO, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridViewListPO
+        '
+        Me.DataGridViewListPO.AllowUserToAddRows = False
+        Me.DataGridViewListPO.AllowUserToDeleteRows = False
+        Me.DataGridViewListPO.AllowUserToResizeColumns = False
+        Me.DataGridViewListPO.AllowUserToResizeRows = False
+        DataGridViewCellStyle5.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        DataGridViewCellStyle5.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle5.ForeColor = System.Drawing.Color.Black
+        DataGridViewCellStyle5.SelectionBackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        DataGridViewCellStyle5.SelectionForeColor = System.Drawing.Color.Black
+        Me.DataGridViewListPO.AlternatingRowsDefaultCellStyle = DataGridViewCellStyle5
+        Me.DataGridViewListPO.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells
+        Me.DataGridViewListPO.BackgroundColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.DataGridViewListPO.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.[Single]
+        DataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter
+        DataGridViewCellStyle6.BackColor = System.Drawing.Color.CornflowerBlue
+        DataGridViewCellStyle6.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle6.ForeColor = System.Drawing.Color.White
+        DataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.CornflowerBlue
+        DataGridViewCellStyle6.SelectionForeColor = System.Drawing.Color.White
+        DataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.DataGridViewListPO.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle6
+        Me.DataGridViewListPO.ColumnHeadersHeight = 20
+        Me.DataGridViewListPO.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing
+        Me.DataGridViewListPO.Location = New System.Drawing.Point(12, 12)
+        Me.DataGridViewListPO.Name = "DataGridViewListPO"
+        Me.DataGridViewListPO.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.[Single]
+        DataGridViewCellStyle7.BackColor = System.Drawing.Color.White
+        DataGridViewCellStyle7.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle7.ForeColor = System.Drawing.Color.Black
+        DataGridViewCellStyle7.SelectionBackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        DataGridViewCellStyle7.SelectionForeColor = System.Drawing.Color.Black
+        Me.DataGridViewListPO.RowHeadersDefaultCellStyle = DataGridViewCellStyle7
+        Me.DataGridViewListPO.RowHeadersVisible = False
+        DataGridViewCellStyle8.BackColor = System.Drawing.Color.White
+        DataGridViewCellStyle8.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle8.ForeColor = System.Drawing.Color.Black
+        DataGridViewCellStyle8.SelectionBackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        DataGridViewCellStyle8.SelectionForeColor = System.Drawing.Color.Black
+        Me.DataGridViewListPO.RowsDefaultCellStyle = DataGridViewCellStyle8
+        Me.DataGridViewListPO.Size = New System.Drawing.Size(633, 194)
+        Me.DataGridViewListPO.TabIndex = 0
+        '
+        'ButtonOk
+        '
+        Me.ButtonOk.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonOk.Location = New System.Drawing.Point(489, 225)
+        Me.ButtonOk.Name = "ButtonOk"
+        Me.ButtonOk.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonOk.TabIndex = 1
+        Me.ButtonOk.Text = "Ok"
+        Me.ButtonOk.UseVisualStyleBackColor = True
+        '
+        'ButtonCancel
+        '
+        Me.ButtonCancel.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonCancel.Location = New System.Drawing.Point(570, 225)
+        Me.ButtonCancel.Name = "ButtonCancel"
+        Me.ButtonCancel.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonCancel.TabIndex = 2
+        Me.ButtonCancel.Text = "Cancel"
+        Me.ButtonCancel.UseVisualStyleBackColor = True
+        '
+        'CheckBoxCheckAll
+        '
+        Me.CheckBoxCheckAll.AutoSize = True
+        Me.CheckBoxCheckAll.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CheckBoxCheckAll.Location = New System.Drawing.Point(12, 231)
+        Me.CheckBoxCheckAll.Name = "CheckBoxCheckAll"
+        Me.CheckBoxCheckAll.Size = New System.Drawing.Size(72, 17)
+        Me.CheckBoxCheckAll.TabIndex = 3
+        Me.CheckBoxCheckAll.Text = "Select All"
+        Me.CheckBoxCheckAll.UseVisualStyleBackColor = True
+        '
+        'SearchPOForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(657, 262)
+        Me.Controls.Add(Me.CheckBoxCheckAll)
+        Me.Controls.Add(Me.ButtonCancel)
+        Me.Controls.Add(Me.ButtonOk)
+        Me.Controls.Add(Me.DataGridViewListPO)
+        Me.Name = "SearchPOForm"
+        Me.Text = "Search Purchase Order (PO)"
+        CType(Me.DataGridViewListPO, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents DataGridViewListPO As System.Windows.Forms.DataGridView
+    Friend WithEvents ButtonOk As System.Windows.Forms.Button
+    Friend WithEvents ButtonCancel As System.Windows.Forms.Button
+    Friend WithEvents CheckBoxCheckAll As System.Windows.Forms.CheckBox
+End Class
